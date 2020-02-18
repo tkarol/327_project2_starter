999fc89320e04a26403de5c3ebbfb1a0727ec61b
Tyler Karol <tyler.karol.17@cnu.edu>
2020-02-17 23:49:58 -0800

    added required functions

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -6,7 +6,14 @@
  */
 
 //============================================================================
-//	TODO add necessary includes here
+
+#define ARRAY_FUNCTIONS_H_
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "constants.h"
 //============================================================================
 
 //============================================================================
@@ -22,3 +29,36 @@
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
 
+void cleararray(){
+
+}
+int getArraySize(){
+	return 0;
+}
+std::string getArrayWordAt(int i){
+	return 0;
+}
+int getArrayWord_NumbOccur_At(int i){
+	return 0;
+}
+bool processFile(std::fstream &myfstream){
+	return false;
+}
+void processLine(std::string &myString{
+
+}
+void processToken(std::string &token){
+
+}
+bool openFile(std::fstream& myfile, const std::string& myFileName, std::ios_base::openmode mode = std::ios_base::in){
+	return false;
+}
+void closeFile(std::fstream& myfile){
+
+}
+int writeArraytoFile(const std::string &outputfilename){
+	return 0;
+}
+void sortArray(constants::sortOrder so){
+
+}
